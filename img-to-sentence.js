56a7750617d9e9b4d99c17d710d35a64e77f01cf
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-05-19 00:48:15 -0400

    breaking fork into its own folder and creating pattern at 1px

--- img-to-sentence.js
+++ img-to-sentence.js
@@ -1,5 +1,5 @@
 var getPixels = require("get-pixels");
-var pattern = require("./pattern.json");
+var pattern = require("./fork/pattern.json");
  
 getPixels(process.argv[2], function(err, pixels) {
   if(err) {
@@ -7,69 +7,29 @@ getPixels(process.argv[2], function(err, pixels) {
     return
   }
 
-  var bits = [];
+  var list = [];
   var sentence = [];
 
   var ll = 640;
-  var bs = 8*4;
+  var bs = 4;
 
   for(var i=0; i<pattern.length; i++){
-  	var seq = pattern[i];
-  	var bit = [];
-  	for(var j=0; j<seq.length; j++){
-  		var block = seq[j];
+  		var block = pattern[i];
   		var x = block[0];
   		var y = block[1];
   		var pos = (y*ll*4)+(x*bs);
 
-  		var r = []
-  		var g = [];
-  		var b = [];
+  		var r = Math.floor(pixels.data[pos]/10)*10;
+      var g = Math.floor(pixels.data[pos+1]/10)*10;
+      var b = Math.floor(pixels.data[pos+2]/10)*10;
 
-  		for(var yy=0; yy<8; yy++){
-  			for(var xx=0; xx<8; xx++){
-  				var k = pos+(yy*ll*4)+(xx*4);
-  				r.push(pixels.data[k]);
-  				g.push(pixels.data[k+1]);
-  				b.push(pixels.data[k+2]);
-  			}
-  		}
-
-  		var ra = Math.floor(r.reduce(function(b,n){ return b+n; }) / r.length);
-  		var ga = Math.floor(g.reduce(function(b,n){ return b+n; }) / g.length);
-  		var ba = Math.floor(b.reduce(function(b,n){ return b+n; }) / b.length);
-
-  		r.sort(function(a,b){
-  			var av = Math.abs(a-ra);
-  			var bv = Math.abs(b-ra);
-  			return bv-av;
-  		});
-
-  		g.sort(function(a,b){
-  			var av = Math.abs(a-ga);
-  			var bv = Math.abs(b-ga);
-  			return bv-av;
-  		});
-
-  		b.sort(function(a,b){
-  			var av = Math.abs(a-ba);
-  			var bv = Math.abs(b-ba);
-  			return bv-av;
-  		});
-
-  		var rv = Math.ceil((r[0]+ra)/2);
-  		var gv = Math.ceil((g[0]+ga)/2);
-  		var bv = Math.ceil((b[0]+ba)/2);
-
-  		var rgb = "rgb("+rv+","+gv+","+bv+")";
-  		bit.push([block[0],block[1],rgb]);
+  		var rgb = "rgb("+r+","+g+","+b+")";
+  		list.push([block[0],block[1],rgb]);
   		sentence.push(rgb);
-  	}
-  	bits.push(bit);
   }
 
-  if(process.argv[3]==="bits"){
-    console.log("window.bits = "+JSON.stringify(bits));
+  if(process.argv[3]==="list"){
+    console.log("window.list = "+JSON.stringify(list));
   }
   else{
     console.log(JSON.stringify(sentence));
