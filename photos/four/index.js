100afd0e906321c8f88ae3f700d3916e3b166c92
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-05-27 14:48:45 -0400

    image four, markov-block bassed fork pattern on the same image set as one

--- photos/four/index.js
+++ photos/four/index.js
@@ -1,14 +1,15 @@
 var co = require("co");
-var processImages = require("../../lib/image-tokenizer/markov-chain");
+var processImages = require("../../lib/image-tokenizer/markov-blocks");
 var buildImage = require("../../lib/build-image");
 
 var listOfImages = require("./source-images.json");
 var db = require("../../lib/db")("./gpm");
 var pattern = require("../../patterns/fork/pattern.json");
 var pixelPicker = require("./pixel-picker");
-var phraseLength = 6;
+var phraseLength = 25;
 var destPath = "./img.jpg";
-var seed = "a0-b0-90.a0-b0-90.a0-b0-a0.a0-b0-90.a0-b0-a0.a0-b0-a0";
+var seed = "0-10-10.10-10-10.0-10-10.20-20-20.10-10-10.10-10-10.10-10-10.20-20-20.20-20-20.20-20-20.10-10-10.10-10-10.10-10-10.10-10-10.10-10-10.10-0-10.10-10-10.10-10-10.10-10-10.10-10-10.10-10-10.10-10-10.10-10-10.10-10-10.10-10-10";
+var fs = require("fs");
 
 
 //I THINK THIS MIGHT REALLY BE LESS RED
@@ -21,33 +22,7 @@ co(function*(){
 		var red = getRed(key);
 		var green = getGreen(key);
 		return Math.floor((green+red)/2);
-	}), db, "./space.jpg");
-	
-	// yield buildImage(pattern, seed, makePixelPicker(function(key){
-	// 	var red = getRed(key);
-	// 	var blue = getBlue(key);
-	// 	return Math.floor((red+blue)/2);
-	// }), db, "./aqua.jpg");
-	
-	// var gen = require("random-seed");
-	// var rand = gen.create("123");
-	// yield buildImage(pattern, seed, makePixelPicker(function(key){
-	// 	return rand(1000);
-	// }), db, "./rand-123.jpg");
-
-	// yield buildImage(pattern, seed, makePixelPicker(function(key){
-	// 	var red = getRed(key);
-	// 	var green = getGreen(key);
-	// 	var blue = getBlue(key);
-	// 	var all = Math.floor((red+green+blue)/3);
-	// 	return 255 - all;
-	// }), db, "./white.jpg");
-	
-	// yield buildImage(pattern, seed, makePixelPicker(function(key){
-	// 	var red = getRed(key);
-	// 	var blue = getBlue(key);
-	// 	return red/blue;
-	// }), db, "./redOverBlue.jpg");
+	}), db, "./space.jpg", phraseLength);
 
 }).catch(function(err){
 	console.error(err.stack);
