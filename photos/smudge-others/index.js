30ac9ec052d92edc70b0543409001b140b3ba151
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-06-03 01:15:22 -0400

    adding in two attempts at blue filters

--- photos/smudge-others/index.js
+++ photos/smudge-others/index.js
@@ -25,11 +25,21 @@ co(function*(){
 	// three = increaseContrast(three, .0001, .9999);
 	// savePixels(three, "jpg").pipe(fs.createWriteStream("./results/three.jpg"));
 
-	var four = yield getPixels("../eight-red/test.jpg");
-	four = smudgeImage(four, box(3));
-	four = increaseContrast(four, .0001, .9999);
-	four = smudgeImage(four, cross(5));
-	savePixels(four, "jpg").pipe(fs.createWriteStream("./results/four.jpg"));
+	// var four = yield getPixels("../eight-red/test.jpg");
+	// four = smudgeImage(four, box(3));
+	// four = increaseContrast(four, .0001, .9999);
+	// four = smudgeImage(four, cross(5));
+	// savePixels(four, "jpg").pipe(fs.createWriteStream("./results/four.jpg"));
+
+	// var five = yield getPixels("../eight-blue/test.jpg");
+	// five = smudgeImage(five, box(3));
+	// savePixels(five, "jpg").pipe(fs.createWriteStream("./results/five.jpg"));
+
+	var six = yield getPixels("../eight-blue/test.jpg");
+	six = smudgeImage(six, box(3));
+	six = smudgeImage(six, box(2));
+	six = smudgeImage(six, box(2));
+	savePixels(six, "jpg").pipe(fs.createWriteStream("./results/six.jpg"));
 
 }).catch(function(err){
 	console.error(err);
