d82814df6e7a0efb47841d903ae08933e99763c2
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-06-28 01:01:13 -0400

    monocrome filter for all

---
+++ sketches/0046/index.js
@@ -0,0 +1,23 @@
+var co = require("co");
+var monocrome = require("../../lib/filters/monocrome");
+var listOfImages = require("./source-images.json");
+var savePixels = require("save-pixels");
+var fs = require("fs");
+var path = require("path");
+
+co(function*(){
+
+	var numImgs = listOfImages.length;
+	console.log(numImgs);
+	
+	for(var b=0; b<numImgs; b++){
+		var imgId = listOfImages[b];
+		var imgPath = path.join(__dirname, "../../instagrams", imgId+".jpg");
+		console.log(imgPath);
+		var img = yield monocrome(imgPath);
+		savePixels(img, "jpg").pipe(fs.createWriteStream("./imgs/"+imgId+".jpg"));
+	}
+
+}).catch(function(err){
+	console.log(err);
+});
