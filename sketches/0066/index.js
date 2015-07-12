9c364ee103e41394f9c25567af298b030492d8ee
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-07-12 12:14:32 -0400

    0066 - node --harmony /index.js 6 10

--- sketches/0066/index.js
+++ sketches/0066/index.js
@@ -10,11 +10,8 @@ var getPixels = require("get-pixels");
 var ndarray = require("ndarray");
 var savePixels = require("save-pixels");
 
-var BLOCK_SIZE = parseInt(process.argv[2]);
-var KEEP_FROM = parseInt(process.argv[3]);
-var ITTERATIONS = parseInt(process.argv[4]);
-
-var KEEP_FROM_IT = (BLOCK_SIZE - KEEP_FROM) / ITTERATIONS;
+var BLOCK_SIZE = parseInt(process.argv[2]) * 3;
+var ITTERATIONS = parseInt(process.argv[3]);
 
 var getBasePixels = function*(imgPath){
 	return new Promise(function(accept, reject){
@@ -31,108 +28,108 @@ var getBasePixels = function*(imgPath){
 
 co(function*(){
 
-	// var numImgs = listOfImages.length;
+	var numImgs = listOfImages.length;
+	var SHIFT_WEIGHT = 1/16;
+	var trainData = [];
+
+	for(var b=1; b<numImgs; b++){
+		var imgId = listOfImages[b];
+		var imgPath = path.join(__dirname, "../../instagrams", imgId+".jpg");
+		console.log("parse "+imgPath+" "+b+" of "+numImgs);
+		var img = yield getBasePixels(imgPath);
+		var fork = pattern(640);
+		var next = fork.next();
+		var current = [];
+		while(next.done === false){
+			var x = next.value[0];
+			var y = next.value[1];
+
+			var red = Math.floor(img.get(x, y, 0) / 16) * SHIFT_WEIGHT;
+			var green = Math.floor(img.get(x, y, 1) / 16) * SHIFT_WEIGHT;
+			var blue = Math.floor(img.get(x, y, 2) / 16) * SHIFT_WEIGHT;
+
+			current.push(red);
+			current.push(green);
+			current.push(blue);
+
+			if(current.length === BLOCK_SIZE){
+				trainData.push([current, current]);
+				current = [];
+			}
+
+			next = fork.next();
+		}
+	}
+
+	console.log("Train!", trainData.length);
+
+	var net = new fann.standard(BLOCK_SIZE, 16, BLOCK_SIZE)
+
+	var info = net.train(trainData, {error: 0.005});
+
+	var imgId = listOfImages[0];
+	var imgPath = path.join(__dirname, "../../instagrams", imgId+".jpg");
+	var img = yield getBasePixels(imgPath);
 	
-	// var trainData = [];
-	// var SHIFT_WEIGHT = 1/16777216;
-	// console.log("BLOCK_SIZE:", BLOCK_SIZE);
+	var pixels = ndarray([], [640, 640, 3]);
 
-	// for(var b=1; b<numImgs; b++){
-	// 	var imgId = listOfImages[b];
-	// 	var imgPath = path.join(__dirname, "../../instagrams", imgId+".jpg");
-	// 	console.log("parse "+imgPath+" "+b+" of "+numImgs);
-	// 	var img = yield getBasePixels(imgPath);
-	// 	var fork = pattern(640);
-	// 	var next = fork.next();
-	// 	var current = [];
-	// 	while(next.done === false){
-	// 		var x = next.value[0];
-	// 		var y = next.value[1];
+	for(var trip=1; trip<ITTERATIONS+1; trip++){
 
-	// 		var red = img.get(x, y, 0);
-	// 		var green = img.get(x, y, 1);
-	// 		var blue = img.get(x, y, 2);
+		var keep = KEEP_FROM - Math.floor(KEEP_FROM_IT * trip);
+		console.log("start", trip, keep);
+		var fork = pattern(640);
+		var next = fork.next();
+		var p = 0;
+		var data = [];
+		var get = [];
+		var xy = [];
+		while(next.done === false){
+			var x = next.value[0];
+			var y = next.value[1];
 
-	// 		current.push((red << 16 | green << 8 | blue) * SHIFT_WEIGHT);
+			var red = Math.floor(img.get(x, y, 0) / 16) * SHIFT_WEIGHT;
+			var green = Math.floor(img.get(x, y, 1) / 16) * SHIFT_WEIGHT;
+			var blue = Math.floor(img.get(x, y, 2) / 16) * SHIFT_WEIGHT;
 
-	// 		if(current.length === BLOCK_SIZE){
-	// 			trainData.push([current, current]);
-	// 			current = [];
-	// 		}
+			get.push(red);
+			get.push(green);
+			get.push(blue);
 
-	// 		next = fork.next();
-	// 	}
-	// }
+			xy.push({
+				x: x,
+				y: y
+			});
 
-	// console.log("Train!", trainData.length);
+			if(get.length == BLOCK_SIZE){
+				var data = net.run(get);
+				for(var i=0; i<data.length; i++){
+					var xx = xy[i].x;
+					var yy = xy[i].y;
+					var val = Math.floor(data[i] / SHIFT_WEIGHT);
 
-	// var net = new fann.standard(BLOCK_SIZE, 8, BLOCK_SIZE)
+					var red = val >> 16;
+					var green = (val - (red << 16)) >> 8;
+					var blue = val - (red << 16) - (green << 8);
 
-	// var info = net.train(trainData, {error: 0.005});
+					pixels.set(xx, yy, 0, red);
+					pixels.set(xx, yy, 1, green);
+					pixels.set(xx, yy, 2, blue);
+				}
 
-	// var imgId = listOfImages[0];
-	// var imgPath = path.join(__dirname, "../../instagrams", imgId+".jpg");
-	// var img = yield getBasePixels(imgPath);
-	
-	// var pixels = ndarray([], [640, 640, 3]);
-	// var stub = BLOCK_SIZE - 10 < 0 ? "00"+BLOCK_SIZE : BLOCK_SIZE - 100 < 0 ? "0"+BLOCK_SIZE : BLOCK_SIZE;
-
-	// for(var trip=1; trip<ITTERATIONS+1; trip++){
-
-	// 	var keep = KEEP_FROM - Math.floor(KEEP_FROM_IT * trip);
-	// 	console.log("start", trip, keep);
-	// 	var fork = pattern(640);
-	// 	var next = fork.next();
-	// 	var p = 0;
-	// 	var data = [];
-	// 	var get = [];
-	// 	var xy = [];
-	// 	while(next.done === false){
-	// 		var x = next.value[0];
-	// 		var y = next.value[1];
-
-	// 		var red = img.get(x, y, 0);
-	// 		var green = img.get(x, y, 1);
-	// 		var blue = img.get(x, y, 2);
-
-	// 		var seed = (red << 16 | green << 8 | blue) * SHIFT_WEIGHT;
-	// 		get.push(seed);
-
-	// 		xy.push({
-	// 			x: x,
-	// 			y: y
-	// 		});
-
-	// 		if(get.length == BLOCK_SIZE){
-	// 			var data = net.run(get);
-	// 			for(var i=0; i<data.length; i++){
-	// 				var xx = xy[i].x;
-	// 				var yy = xy[i].y;
-	// 				var val = Math.floor(data[i] / SHIFT_WEIGHT);
-
-	// 				var red = val >> 16;
-	// 				var green = (val - (red << 16)) >> 8;
-	// 				var blue = val - (red << 16) - (green << 8);
-
-	// 				pixels.set(xx, yy, 0, red);
-	// 				pixels.set(xx, yy, 1, green);
-	// 				pixels.set(xx, yy, 2, blue);
-	// 			}
-
-	// 			get = get.splice(keep);
-	// 			xy = xy.splice(keep);
-	// 		}
+				get = [];
+				xy = [];
+			}
 			
-	// 		next = fork.next();
-	// 	}
+			next = fork.next();
+		}
 
-	// 	if(trip%20 === 0 || trip === 1 || trip === ITTERATIONS){
-	// 		console.log("saving this one");
-	// 		var tripStub = trip - 10 < 0 ? "00"+trip : trip - 100 < 0 ? "0"+trip : trip;
-	// 		savePixels(pixels, "jpg").pipe(fs.createWriteStream("./"+tripStub+".jpg"));
-	// 	}
-	// 	img = pixels;
-	// }
+		if(trip%20 === 0 || trip === 1 || trip === ITTERATIONS){
+			console.log("saving this one");
+			var tripStub = trip - 10 < 0 ? "00"+trip : trip - 100 < 0 ? "0"+trip : trip;
+			savePixels(pixels, "jpg").pipe(fs.createWriteStream("./imgs/"+tripStub+".jpg"));
+		}
+		img = pixels;
+	}
 
 
 }).then(sketchSaver).catch(function(err){
