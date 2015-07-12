00ff491e94ae963d8575608d29426d6a82657d48
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-07-12 10:56:41 -0400

    0061

---
+++ sketches/0061/README.md
@@ -0,0 +1,3 @@
+# 0061
+
+0060 with Fann rather than Brain
---
+++ sketches/0061/fann-test.js
@@ -0,0 +1,22 @@
+// 
+// This neural network calculates XOR function (classic example)
+//
+
+var fann = require('fann')
+var net = new fann.standard(2,10,1)
+
+var data = [
+  [[0, 0], [0]],
+  [[0, 1], [1]],
+  [[1, 0], [1]],
+  [[1, 1], [0]],
+]
+
+net.train(data, { error: 0.00001 })
+
+;[0,1].forEach(function(a) {
+  ;[0,1].forEach(function(b) {
+    var c = net.run([a, b]);
+    console.log("xor test (" + a + "," + b + ") -> " + c)
+  })
+})
