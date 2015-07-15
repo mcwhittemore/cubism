e6f36c0e8f1cddfa550cf67e106d0f785e895055
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-07-14 23:48:04 -0400

    0076 - node --harmony /index.js

--- sketches/0076/index.js
+++ sketches/0076/index.js
@@ -139,9 +139,9 @@ co(function*(){
 
 	for(var x = 0; x<640; x++){
 		for(var y=0; y<640; y++){
-			var red = imgRed.get(x, y, 0);
-			var green = imgGreen.get(x, y, 1);
-			var blue = imgBlue.get(x, y, 2);
+			var red = Math.max(imgRed.get(x, y, 0), imgGreen.get(x, y, 0), imgBlue.get(x, y, 0));
+			var green = Math.max(imgRed.get(x, y, 1), imgGreen.get(x, y, 1), imgBlue.get(x, y, 1));
+			var blue = Math.max(imgRed.get(x, y, 2), imgGreen.get(x, y, 2), imgBlue.get(x, y, 2));
 
 			newImg.set(x, y, 0, red);
 			newImg.set(x, y, 0, green);
