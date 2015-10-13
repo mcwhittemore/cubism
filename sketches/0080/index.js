21f60b6b5001c552a90e2bdb51795675751f8ba0
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-10-12 23:37:21 -0400

    0080 - /Users/whittemore/.nvm/versions/node/v4.1.1/bin/node /index.js

---
+++ sketches/0080/index.js
@@ -0,0 +1,47 @@
+var sketchSaver = require("../../lib/sketch-saver");
+var co = require("co");
+var listOfImages = Object.keys(require("../../image-sets/washington-monument-8-29-15-9-10-15.json"));
+var fs = require("fs");
+var path = require("path");
+var getPixels = require("get-pixels");
+var savePixels = require("save-pixels");
+
+var getBasePixels = function*(imgPath){
+	return new Promise(function(accept, reject){
+		getPixels(imgPath, function(err, pixels) {
+			if(err) {
+				reject(err);
+			}
+			else{
+				accept(pixels);
+			}
+		});
+	});
+}
+
+var getPath = function(imgId){
+	return path.join(__dirname, "../../instagrams", imgId+".jpg");
+}
+
+co(function*(){
+
+	//7LGTA1q57n.jpg
+
+	var imgs = [];
+
+	for(var i=0; i<listOfImages.length; i++){
+		var imgId = listOfImages[i];
+		var imgPath = getPath(imgId);
+		
+		var img = yield getBasePixels(imgPath);
+		
+		
+		imgs.push(img);
+	}
+
+	savePixels(imgs[0], "jpg").pipe(fs.createWriteStream("./end.jpg"));
+
+}).then(sketchSaver).catch(function(err){
+	console.log(err.stack);
+	sketchSaver();
+});
