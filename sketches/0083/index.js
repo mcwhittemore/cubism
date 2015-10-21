731f3408411238b4c67b5067b55d47916172de73
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2015-10-21 03:21:27 -0400

    0083 - /Users/whittemore/.nvm/versions/node/v4.1.1/bin/node /index.js

--- sketches/0083/index.js
+++ sketches/0083/index.js
@@ -11,8 +11,8 @@ var ngraph = require('ngraph.graph');
 var Modularity = require('ngraph.modularity');
 var pixelBuilder = require('./pixel-blocker');
 
-var BLOCK_SIZE = 8;
-var TOP_LINE = 0;
+var BLOCK_SIZE = 16;
+var TOP_LINE = .1;
 var UNIT_BLOCK_SIZE = 4;
 var MIN_IMGS_TO_BE_NODE = 1;
 
