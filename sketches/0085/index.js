ee5124b97aa6d2abe73ac237b01867df1f77247d
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2016-04-15 01:55:13 +0530

    sketch 0085

---
+++ sketches/0085/index.js
@@ -0,0 +1,129 @@
+var path = require('path');
+var co = require("co");
+var ndarray = require('ndarray');
+var ngraph = require('ngraph.graph');
+var forcelayout = require('ngraph.forcelayout.v2');
+
+var colorTools = require('../../lib/color-tools');
+var getBasePixels = require('../../lib/get-base-pixels');
+var getPath = require('../../lib/get-image-path');
+var saveImage = require('../../lib/save-image');
+var sketchSaver = require("../../lib/sketch-saver");
+
+var listOfImages = require("./image-ids.json");
+
+co(function*(){
+
+	var setOfColors = [];
+
+	var theGraph = ngraph();
+
+	console.log('adding images to graph');
+	var count = 0;
+	for(var i=0; i<listOfImages.length; i++){
+		var imgId = listOfImages[i];
+		var imgPath = getPath(imgId);
+		var rawImg = yield getBasePixels(imgPath);
+
+		for (var x = 0; x<640; x+=5) {
+			for (var y = 0; y<640; y+=5) {
+				var red = rawImg.get(x, y, 0);
+				var green = rawImg.get(x, y, 1);
+				var blue = rawImg.get(x, y, 2);
+				var color = colorTools.encode(red, green, blue);
+
+				var id = setOfColors.length
+				setOfColors.push(color);
+				theGraph.addNode(id);
+			}
+		}
+	}
+
+	console.log('connecting nodes');
+	var connections = [300, 500, 800, 1300];
+	for (var i=0; i < setOfColors.length - connections[0]; i++) {
+		var color = setOfColors[i];
+		for (var j=0; j<connections.length; j++) {
+			var nextId = i+connections[j];
+			var next = setOfColors[nextId];
+			if (next) {
+				var score = colorTools.compare(color, next);
+				theGraph.addLink(i, nextId, score);
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	console.log('doing some physics');
+	var layout = forcelayout(theGraph);
+	for (var frame = 0; frame < 3; frame++) {
+		console.log('frame', frame, 'of', 30);
+		layout.step();
+	}
+
+
+	console.log('projecting nodes into image size');
+	var position = layout.getNodePosition(0);
+
+	var minX = position.x;
+	var maxX = position.x;
+
+	var minY = position.y;
+	var maxY = position.y;
+
+	for(var i=1; i<setOfColors.length; i++) {
+		position = layout.getNodePosition(i);
+		if (position.x < minX) {
+			minX = position.x;
+		}
+
+		if (position.x > maxX) {
+			maxX = position.x;
+		}
+
+		if (position.y < minY) {
+			minY = position.y;
+		}
+
+		if (position.y > maxY) {
+			maxY = position.y;
+		}
+	}
+
+	var scale = 640 / Math.max((maxX - minX), (maxY - minY));
+
+	var holdPixels = ndarray([], [640, 640, 4]);
+
+	for (var i = 0; i < setOfColors.length; i++) {
+		var position = layout.getNodePosition(i);
+		var x = Math.floor((position.x - minX) * scale);
+		var y = Math.floor((position.y - minY) * scale);
+		var color = colorTools.decode(setOfColors[i]);
+		color.push(1);
+
+		for(var c = 0; c<4; c++) {
+			holdPixels.set(x, y, c, holdPixels.get(x, y, c) + color[c]);
+		}
+	}
+
+	console.log('merging colors and saving')
+	var pixels = ndarray([], [640, 640, 3]);
+	for (var x = 0; x<640; x++) {
+		for (var y = 0; y<640; y++) {
+			var num = pixels.get(x, y, 3);
+			for (var c = 0; c < 3; c++) {
+				pixels.set(x, y, c, Math.floor(holdPixels.get(x, y, c) / num));
+			}
+		}
+	}
+
+	yield saveImage(pixels, path.join(__dirname, 'data', 'fun.jpg'));
+
+
+}).then(sketchSaver).catch(function(err){
+	console.log(err.message);
+	console.log(err.stack);
+	sketchSaver();
+});
